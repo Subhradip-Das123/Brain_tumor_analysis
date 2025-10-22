82c3a2bc2fd2ec7075ed7bd39e9da56a285b93e7
Subhradip-Das123 <124434372+Subhradip-Das123@users.noreply.github.com>
2025-10-22 21:35:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,16 +14,18 @@ from scipy import ndimage
 # ======================================================
 def load_model():
     """Load trained PyTorch model, download if missing"""
+    import torch, os, gdown
+
     model_path = "model/brain_tumor_model.pth"
     if not os.path.exists(model_path):
         os.makedirs("model", exist_ok=True)
-        file_id = "1qUajdKsWAvqU1L2uP2zsgZVtDwCJsiWi"
+        file_id = "1qUajdKsWAvqU1L2uP2zsgZVtDwCJsiWi"  # ✅ your file ID
         url = f"https://drive.google.com/uc?export=download&id={file_id}"
         gdown.download(url, model_path, quiet=False)
+
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model.eval()
     return model
-
 
 # ======================================================
 # 2️⃣ Preprocess and Predict
@@ -202,4 +204,5 @@ def load_nifti_volume(nifti_path):
     except Exception as e:
         print(f"Error loading NIfTI file: {e}")
         return None
+
 
